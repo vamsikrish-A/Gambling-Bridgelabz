1af0c6ffb3b97605ba12740ab2b67f046440ba68
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-13 09:20:12 +0530

    first commit

---
+++ src/com/bridgelabz/Gamblingsimulator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Gamblingsimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to game simulator");
+        final int STAKEPERDAY = 100;
+        final int BETPERGAME = 1;
+    }
+}
+
